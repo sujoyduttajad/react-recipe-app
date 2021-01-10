3f246fe90d74ba0b9a0a559922ecf48d522efd88
sujoyduttajad <sujoyduttajad@gmail.com>
2021-01-10 12:57:59 +0530

    first commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react'
+import {API_ID, API_KEY} from '../lib/API_KEY'
+import SingleRecipe from './SingleRecipe'
+
+
+const Recipe = () => {
+
+    const APP_ID = API_ID;
+    const APP_KEY = API_KEY;
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('chicken');
+
+    useEffect(() => {
+        getRecipes();
+    }, [query]);
+
+    const getRecipes = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+        setRecipes(data.hits);
+    }
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
+    const getSearch = (e) => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
+    }
+    return (
+        <div>
+            <form onSubmit={getSearch} className="search-form">
+                <h1 className="header">Recipe App</h1>
+                <input 
+                    className="search-bar" 
+                    type="text" 
+                    value={search} 
+                    onChange={handleSearch}
+                    placeholder="Search any recipe"
+                />
+                <button className="search-button" type="submit">Search</button>
+            </form>
+            <div className="recipes">
+                {recipes.map((recipe, index) => (
+                    
+                        <SingleRecipe key={index} 
+                            title={recipe.recipe.label}
+                            calories={recipe.recipe.calories}
+                            image={recipe.recipe.image}
+                            ingredients={recipe.recipe.ingredients}
+                        />
+                    
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Recipe
---
+++ src/components/SingleRecipe.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import style from './SingleRecipe.module.css'
+
+
+const SingleRecipe = ({title, calories, image, ingredients}) => {
+    return (
+
+        <div className={style.recipe}>
+            <h1 className={style.header}>{title}</h1>
+            <img className={style.image} src={image} alt={title} /> 
+            <h2>Calories - {Math.floor(calories)}</h2>
+            <ol><strong>Ingredients -</strong>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+        </div>
+    )
+}
+
+export default SingleRecipe
