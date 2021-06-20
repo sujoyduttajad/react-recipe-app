c249c868749410b67e178ef554195b0397cd6ad2
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-20 19:06:58 +0530

    testing api objects

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -18,6 +18,7 @@ const Recipe = () => {
     const getRecipes = async () => {
         const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
         const data = await response.json();
+        console.log(data);
         setRecipes(data.hits);
     }
 
