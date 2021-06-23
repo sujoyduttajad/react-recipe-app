15df1ab06a8a54556556e83d4555b17b5b070fd7
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-24 03:30:18 +0530

    added a Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import Recipe from './components/Recipe'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <div className="App">
+      <Sidebar />
       <Recipe /> 
     </div>
   );
---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <section>
+            <div><h3>Library</h3></div>
+            <div><h3>Library</h3></div>
+            <div><h3>Library</h3></div>
+            <div><h3>Library</h3></div>
+            <div><h3>Library</h3></div>
+
+            <div><h3>Library</h3></div>
+            <div><h3>Library</h3></div>
+
+            <div><h3>Library</h3></div>
+            </section>
+        </aside>
+    )
+}
+
+export default Sidebar
