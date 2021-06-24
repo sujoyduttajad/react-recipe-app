04ca30ad2c93cd07308cb1f7faeaf3b09067ed7d
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-24 18:52:42 +0530

    fixed the calorie heading styles

--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -8,7 +8,7 @@ const SingleRecipe = ({title, calories, image, ingredients}) => {
             <img className='image' src={image} alt={title} /> 
             <div>
                 <h1 className='recipe-header'>{title.length > 10 ? title.slice(0, 10)+'...' : title}</h1>
-                <h2 className="calories-header">Calories - {Math.floor(calories)}kcal</h2>
+                <h2 className="calories-header">{Math.floor(calories)}kcal</h2>
             </div>
             {/* <ol><strong>Ingredients -</strong>
                 {ingredients.map(ingredient => (
