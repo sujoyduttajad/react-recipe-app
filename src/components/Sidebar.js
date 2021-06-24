301d465eb1ac2cafbf9d371edab19cc6b2546a06
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-24 16:18:45 +0530

    added icons for the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,34 @@
 import React from 'react'
-
+import { Category, Document, Heart, Graph, Setting, InfoCircle } from 'react-iconly';
 
 const Sidebar = () => {
     return (
         <aside>
             <section className="slideout">
-            <div><h3>Overview</h3></div>
-            <div><h3>Recipes</h3></div>
-            <div><h3>Favourites</h3></div>
-            <div><h3>Settings</h3></div>
-            <div><h3>Help</h3></div>
+            <div className="icon-container">
+                <Category name="category" set="light border" primaryColor="white" size="large"/>
+                <h3>Overview</h3>
+            </div>
+            <div className="icon-container">
+                <Document name="category" set="light border" primaryColor="white" size="large"/>
+                <h3>Recipes</h3>
+            </div>
+            <div className="icon-container">
+                <Heart name="category" set="light border" primaryColor="white" size="large"/>
+                <h3>Favourites</h3>
+            </div>
+            <div className="icon-container">
+                <Graph name="category" set="light border" primaryColor="white" size="large"/>
+                <h3>Community</h3>
+            </div>
+            <div className="icon-container">
+                <Setting name="category" set="light border" primaryColor="white" size="large"/>
+                <h3>Settings</h3>
+            </div>
+            <div className="icon-container">
+                <InfoCircle name="category" set="light border" primaryColor="white" size="large"/>
+                <h3>Help</h3>
+            </div>
             </section>
         </aside>
     )
