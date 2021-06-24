137648dbf45c87083598fc3666dafdc4aaa8181f
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-24 21:42:52 +0530

    changed spacing of calorie count

---
+++ src/components/TrendSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const TrendSection = () => {
+    return (
+        <aside>
+            
+            <section className="slideout-right">
+                <div>
+                    <h1>Right</h1>
+                    <h1>Right</h1>
+                    <h1>Right</h1>
+                    <h1>Right</h1>
+                    <h1>Right</h1>
+                    <h1>Right</h1>
+                    <h1>Right</h1>
+                    <h1>Right</h1>
+                </div>
+            </section>
+        </aside>
+    )
+}
+
+export default TrendSection
