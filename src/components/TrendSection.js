d8d35b49cebb1f1ec4302bf0effd32d9de27dfa6
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-24 22:45:58 +0530

    added upload button and new add image

--- src/components/TrendSection.js
+++ src/components/TrendSection.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import add from '../images/food-add.svg'
+
 
 const TrendSection = () => {
     return (
@@ -6,6 +8,10 @@ const TrendSection = () => {
             
             <section className="slideout-right">
                 <div className='add-section'>
+                    <img src={add} alt='upload your recipe' />
+                    <div>
+                    <button className='upload-recipe-button'>Upload</button>
+                    </div>
                     
                 </div>
                 <div className='trend-section'>
