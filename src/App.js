8d7cfceb85f3df307108572a2e3fd4377585a48e
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-24 15:38:21 +0530

    added sass and new fonts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react'
 import './App.css';
 import Recipe from './components/Recipe'
 import Sidebar from './components/Sidebar'
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
-    <div className="App">
-      <Sidebar />
-      <Recipe /> 
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3">
+          <div className="">
+            <Sidebar />
+             
+          </div>
+        </div>
+        <div className="col-lg-6">
+          <div className="App">
+          <Recipe />
+          </div>
+        </div>
+        <div className="col-lg-3">
+          <div className="App">
+          
+          </div>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
