871e3f49293f51e41569c91c335bff90edba558d
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-26 08:44:17 +0530

    changed the box-shadows and made icons bold

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,27 +13,27 @@ const Sidebar = () => {
                     <img src={logo} alt="logo" />
                 </div>
                 <div className="icon-container">
-                    <Category name="category" set="light border" primaryColor="white" size="large"/>
+                    <Category stroke='bold' name="category" set="light border" primaryColor="white" size="large"/>
                     <h3>Overview</h3>
                 </div>
                 <div className="icon-container">
-                    <Document name="category" set="light border" primaryColor="white" size="large"/>
+                    <Document stroke='bold' name="category" set="light border" primaryColor="white" size="large"/>
                     <h3>Recipes</h3>
                 </div>
                 <div className="icon-container">
-                    <Heart name="category" set="light border" primaryColor="white" size="large"/>
+                    <Heart stroke='bold' name="category" set="light border" primaryColor="white" size="large"/>
                     <h3>Favourites</h3>
                 </div>
                 <div className="icon-container">
-                    <Graph name="category" set="light border" primaryColor="white" size="large"/>
+                    <Graph stroke='bold' name="category" set="light border" primaryColor="white" size="large"/>
                     <h3>Community</h3>
                 </div>
                 <div className="icon-container">
-                    <Setting name="category" set="light border" primaryColor="white" size="large"/>
+                    <Setting stroke='bold' name="category" set="light border" primaryColor="white" size="large"/>
                     <h3>Settings</h3>
                 </div>
                 <div className="icon-container">
-                    <InfoCircle name="category" set="light border" primaryColor="white" size="large"/>
+                    <InfoCircle stroke='bold' name="category" set="light border" primaryColor="white" size="large"/>
                     <h3>Help</h3>
                 </div>
             </section>
