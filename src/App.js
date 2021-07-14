3203a898e15cd859086cdff440d8cc94ac5e0ec6
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-14 10:21:40 +0530

    API issue detected provider is working on it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import HomeRecipe from './components/HomeRecipe'
 import RecipePage from './components/RecipePage'
-import { API_KEY } from './lib/API_KEY.js'
+import { API_ID, ED_API_KEY ,API_KEY } from './lib/API_KEY.js'
 import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
 
-  // const APP_ID = API_ID;
+  const APP_ID = API_ID;
+  const ED_APP_KEY = ED_API_KEY;
   const APP_KEY = API_KEY;
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState('');
@@ -18,22 +19,22 @@ function App() {
 
   const getRecipes = async () => {
       // const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-      const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=100`);
+      const response = await fetch(`https://api.spoonacular.com/food/complexSearch?query=${query}?&apiKey=${APP_KEY}&number=100`);
       const data = await response.json();
       console.log(data);
-      setRecipes(data.searchResults[0].results);
+      setRecipes(data.searchResults.results);
   }
 
   const [dietType, setDietType] = useState([]);
 
   useEffect(() => {
     getTrendingRecipes();
-  }, [dietType]);
+  }, []);
 
   const getTrendingRecipes = async () => {
     
-    const responseDiet = await fetch(`https://api.spoonacular.com/recipes/complexSearch?&apiKey=${APP_KEY}&number=4&diet=Ketogenic`);
-    let dietData = await responseDiet.json();
+    const response = await fetch(`https://api.edamam.com/search?q&app_id=${APP_ID}&app_key=${ED_APP_KEY}`);
+    const dietData = await response.json();
     console.log(dietData);
     setDietType(dietData.results);
 }
--- src/components/TrendSection.js
+++ src/components/TrendSection.js
@@ -4,8 +4,8 @@ import add from '../images/food-add.svg'
 
 const TrendSection = ({ dietType }) => {
 
-    const healthyRecipes = dietType;
-    // console.log(healthyRecipes)
+    // const healthyRecipes = dietType.filter(recipe => recipe.recipe.dietLabels[0] === 'Low-Carb' ? recipe.recipe.label : '');
+    console.log(dietType)
 
 
     return (
@@ -21,14 +21,14 @@ const TrendSection = ({ dietType }) => {
                 </div>
                 <div className='trend-section'>
                     <h1>Healthy recipes</h1>
-                    {
+                    {/* {
                         healthyRecipes.map(healthy => (
                             <div className="healthy-recipes">
-                                <img src={healthy.image} alt={healthy.title} />
-                                <div className="healthy-recipe-label"><p>{healthy.title}</p></div>
+                                <img src={healthy.recipe.image} alt={healthy.recipe.label} />
+                                <div className="healthy-recipe-label"><p>{healthy.recipe.label}</p></div>
                             </div>
                         ))
-                    }                 
+                    }                  */}
                 </div>
             </section>
         </aside>
