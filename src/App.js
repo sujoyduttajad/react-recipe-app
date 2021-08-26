d3b420082514908f3d9048848a219f0e6ec2c298
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-26 12:46:12 +0530

    Updated the API

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   const getRecipes = async () => {
       // const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
       // const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10`);
-      const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10&diet=keto`);
+      const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10`);
       const data = await response.json();
       console.log(data);
       setRecipes(data.searchResults[0].results);
--- src/components/SingleRecipe.js
+++ src/components/SingleRecipe.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SingleRecipe = ({ recipes }) => {
 
-    console.log(recipes);
+    console.log(recipes.id);
     return (
         <div>
             <div>
