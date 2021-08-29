548bd77ea02a45be77fe7e9bd12349138ac29264
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-29 18:24:26 +0530

    Updated RecipePage

--- src/components/CardRecipe.js
+++ src/components/CardRecipe.js
@@ -45,7 +45,7 @@ const CardRecipe = ({title, calories, image, id}) => {
                         </HtmlTooltip>
                         <h2 className="calories-header">{
                             // console.log(calories.match((^<b></b>/\d\skcal/$)))
-                            console.log(calories)
+                            // console.log(calories)
                             } kcal</h2>
                     </div>
                     
