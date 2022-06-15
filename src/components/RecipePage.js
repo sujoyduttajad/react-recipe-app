2e3e4da601a91ae7c50647f9c8134eacce918afa
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-15 15:31:21 +0530

    Update RecipePage.js

--- src/components/RecipePage.js
+++ src/components/RecipePage.js
@@ -1,37 +1,27 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import TrendSection from './TrendSection'
-import Recipe from './Recipe';
+import React from "react";
+import Sidebar from "./Sidebar";
+import TrendSection from "./TrendSection";
+import Recipe from "./Recipe";
 
-function RecipePage({ search, setSearch, setQuery, recipes, dietType}) {
-
-    
+function RecipePage({ search, setSearch, setQuery, recipes, dietType }) {
   return (
-    <div className="container">
-      <div className="row">
-        
-          <div className="sidebar">
-            <Sidebar />           
-          </div>
-        
-          <div className="body">
-            <Recipe 
-                  setSearch={setSearch}
-                  search={search}
-                  setQuery={setQuery}
-                  recipes={recipes} 
-              />
-          </div>
-        
-          <div className="add">
-            <TrendSection  dietType={dietType} />
-          </div>
-        
+    <div className="row">
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <div className="body">
+        <Recipe
+          setSearch={setSearch}
+          search={search}
+          setQuery={setQuery}
+          recipes={recipes}
+        />
+      </div>
+      <div className="add">
+        <TrendSection dietType={dietType} />
       </div>
     </div>
-  )
-
+  );
 }
-
 
 export default RecipePage;
