fb1ba576785a8f0212f5d28142f107e53dd6a6fd
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-15 15:31:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React, {useState, useEffect} from 'react'
-import './styles/app.scss';
-import HomeRecipe from './components/HomeRecipe'
-import RecipePage from './components/RecipePage'
-import { API_ID, ED_API_KEY ,API_KEY } from './lib/API_KEY.js'
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./styles/app.scss";
+import HomeRecipe from "./components/HomeRecipe";
+import RecipePage from "./components/RecipePage";
+import { API_ID, ED_API_KEY, API_KEY } from "./lib/API_KEY.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-
   const APP_ID = API_ID;
   const ED_APP_KEY = ED_API_KEY;
   const APP_KEY = API_KEY;
   const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('');
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-      getRecipes();
+    getRecipes();
   }, [query]);
 
   const getRecipes = async () => {
-      // const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-      // const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10`);
-      const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10`);
-      const data = await response.json();
-      console.log(data);
-      setRecipes(data.searchResults[0].results);
-  }
+    // const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    // const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10`);
+    const response = await fetch(
+      `https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10`
+    );
+    const data = await response.json();
+    console.log(data);
+    setRecipes(data.searchResults[0].results);
+  };
 
   const [dietType, setDietType] = useState([]);
 
@@ -34,41 +35,48 @@ function App() {
   }, []);
 
   const getTrendingRecipes = async () => {
-    
-    const response = await fetch(`https://api.edamam.com/search?q&app_id=${APP_ID}&app_key=${ED_APP_KEY}&diet=balanced&limit=5`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q&app_id=${APP_ID}&app_key=${ED_APP_KEY}&diet=balanced&limit=5`
+    );
     const dietData = await response.json();
     // console.log(dietData.hits)
     setDietType(dietData.hits);
     console.log(dietType);
-}
+  };
 
   return (
     <>
       <Router>
         <Switch>
-          <Route path='/' exact component={() => (
-                      <HomeRecipe 
-                        setSearch={setSearch}
-                        setQuery={setQuery}
-                        search={search}
-                        recipes={recipes}
-                        dietType={dietType}
-                      /> )}
+          <Route
+            path="/"
+            exact
+            component={() => (
+              <HomeRecipe
+                setSearch={setSearch}
+                setQuery={setQuery}
+                search={search}
+                recipes={recipes}
+                dietType={dietType}
+              />
+            )}
           />
-          <Route path='/recipes' exact component={() => (
-                      <RecipePage 
-                        setSearch={setSearch}
-                        setQuery={setQuery}
-                        search={search}
-                        recipes={recipes}
-                        dietType={dietType}
-                      /> )}
+          <Route
+            path="/recipes"
+            exact
+            component={() => (
+              <RecipePage
+                setSearch={setSearch}
+                setQuery={setQuery}
+                search={search}
+                recipes={recipes}
+                dietType={dietType}
+              />
+            )}
           />
         </Switch>
       </Router>
-      
     </>
-    
   );
 }
 
