26d9fd6b47d9d3c04f4260253f8f42156d6f02f8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-15 16:06:52 +0530

    Update CardRecipe.js

--- src/components/CardRecipe.js
+++ src/components/CardRecipe.js
@@ -1,31 +1,28 @@
-import React, {useState} from 'react'
-import Tooltip from '@material-ui/core/Tooltip';
-import { withStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import Tooltip from "@material-ui/core/Tooltip";
+import { withStyles } from "@material-ui/core/styles";
 import { NavLink } from "react-router-dom";
-import SingleRecipe from './SingleRecipe'
-
+import SingleRecipe from "./SingleRecipe";
 
 const HtmlTooltip = withStyles((theme) => ({
-    tooltip: {
-      backgroundColor: '#D6D6D6',
-      color: 'rgba(0, 0, 0, 0.87)',
-      maxWidth: 220,
-      fontSize: theme.typography.pxToRem(12),
-      border: '1px solid #dadde9',
-    },
-  }))(Tooltip);
+  tooltip: {
+    backgroundColor: "#D6D6D6",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: "1px solid #dadde9",
+  },
+}))(Tooltip);
 
-const CardRecipe = ({title, calories, image, id}) => {
+const CardRecipe = ({ title, calories, image, id }) => {
+  const [recipeID, setRecipeID] = useState(0);
 
-    const [recipeID, setRecipeID] = useState(0);
-    
+  const handleRecipeClick = (id) => {
+    setRecipeID(id);
+    console.log(recipeID);
+  };
 
-    const handleRecipeClick = (id) => {
-        setRecipeID(id);
-        console.log(recipeID);
-    }
-
-/* ########     Stratergy     ########
+  /* ########     Stratergy     ########
     The idea is to get the recipe_id when the user clicks(onClick) and then pass the id to 
     the SingleRecipe.js then fetch another Spoonacular API which shows all the recipe 
     information based on id. Then from there, we can get access to all the required information. 
@@ -39,41 +36,40 @@ const CardRecipe = ({title, calories, image, id}) => {
     I also need to get the SingleRecipe.js called inside the CardRecipe.js    
 */
 
-    return (
-        <NavLink to='/recipes' > 
-            <div className='recipe'>
-                <HtmlTooltip arrow  title={title} placement="bottom">
-                    <img className='image' src={image} alt={title} />
-                </HtmlTooltip>
-                <div className="recipe-name-container">
-                    <div className="recipe-calories-name">
-                        <HtmlTooltip arrow  title={title} placement="left">
-                            <h1 className='recipe-header'>{title.length > 20 ? title.slice(0, 20)+'...' : title}</h1>
-                        </HtmlTooltip>
-                        <h2 className="calories-header">{
-                            // console.log(calories.match((^<b></b>/\d\skcal/$)))
-                            // console.log(calories)
-                            } kcal</h2>
-                    </div>
-                    
-                    <NavLink to={`/recipes/${recipeID}`} > 
-                        <button 
-                            className='more-recipe-button'
-                            onClick={() => handleRecipeClick(id)}
-                        >
-                            Show More
-                            { recipeID > 0 ?
-                                <SingleRecipe 
-                                    recipeID={recipeID} 
-                                />
-                                : ''
-                            }
-                        </button>
-                    </NavLink>
-                </div>
-            </div>
-        </NavLink>
-    )
-}
+  return (
+    // <NavLink to="/recipes">
+    <div className="recipe">
+      <HtmlTooltip arrow title={title} placement="bottom">
+        <img className="image" src={image} alt={title} />
+      </HtmlTooltip>
+      <div className="recipe-name-container">
+        <div className="recipe-calories-name">
+          <HtmlTooltip arrow title={title} placement="left">
+            <h1 className="recipe-header">
+              {title.length > 20 ? title.slice(0, 20) + "..." : title}
+            </h1>
+          </HtmlTooltip>
+          <h2 className="calories-header">
+            {
+              // console.log(calories.match((^<b></b>/\d\skcal/$)))
+              // console.log(calories)
+            }{" "}
+            kcal
+          </h2>
+        </div>
+        <button
+          className="more-recipe-button"
+          onClick={() => handleRecipeClick(id)}
+        >
+          <NavLink to={`/recipes/${recipeID}`}>
+            Show More
+            {recipeID > 0 ? <SingleRecipe recipeID={recipeID} /> : ""}
+          </NavLink>
+        </button>
+      </div>
+    </div>
+    // </NavLink>
+  );
+};
 
-export default CardRecipe
+export default CardRecipe;
