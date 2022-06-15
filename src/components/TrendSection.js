acba04d2885f01d2e1ac54e51ed1245a5fbad9d2
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-15 16:06:49 +0530

    Update TrendSection.js

--- src/components/TrendSection.js
+++ src/components/TrendSection.js
@@ -1,40 +1,35 @@
-import React from 'react'
-import add from '../images/food-add.svg'
-
+import React from "react";
+import add from "../images/food-add.svg";
 
 const TrendSection = ({ dietType }) => {
+  // const healthyRecipes = dietType.filter(recipe => recipe.recipe.dietLabels[0] === 'Low-Carb' ? recipe.recipe.label : '');
+  console.log(dietType);
 
-    // const healthyRecipes = dietType.filter(recipe => recipe.recipe.dietLabels[0] === 'Low-Carb' ? recipe.recipe.label : '');
-    console.log(dietType)
+  return (
+    <aside>
+      <section className="slideout-right">
+        <div className="add-section">
+          <img src={add} alt="upload your recipe" />
+          <div>
+            <button className="upload-recipe-button">Upload</button>
+          </div>
+        </div>
+        <div className="trend-section">
+          <div className="trend-header">
+            <h1>Healthy recipes</h1>
+          </div>
+          {dietType.slice(0, 4).map((healthy) => (
+            <div className="healthy-recipes">
+              <img src={healthy.recipe.image} alt={healthy.recipe.label} />
+              <div className="healthy-recipe-label">
+                <p>{healthy.recipe.label}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </aside>
+  );
+};
 
-
-    return (
-        <aside>
-            
-            <section className="slideout-right">
-                <div className='add-section'>
-                    <img src={add} alt='upload your recipe' />
-                    <div>
-                    <button className='upload-recipe-button'>Upload</button>
-                    </div>
-                    
-                </div>
-                <div className='trend-section'>
-                    <div className='trend-header'><h1>Healthy recipes</h1></div>
-                    {
-                        dietType.slice(0,4).map(healthy => (
-                            <div className="healthy-recipes">
-                                <img src={healthy.recipe.image} alt={healthy.recipe.label} />
-                                <div className="healthy-recipe-label">
-                                    <p>{healthy.recipe.label}</p>
-                                </div>
-                            </div>
-                        ))
-                    } 
-                </div>
-            </section>
-        </aside>
-    )
-}
-
-export default TrendSection
+export default TrendSection;
