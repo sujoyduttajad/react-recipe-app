ce6905eadde36b3887c732714047327b1d9663ab
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-06-20 13:32:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,19 @@ import { API_ID, ED_API_KEY, API_KEY } from "./lib/API_KEY.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  // states
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("");
-  const [dietType, setDietType] = useState([]);
-
-  // API keys
+  
   const APP_ID = API_ID;
   const ED_APP_KEY = ED_API_KEY;
   const APP_KEY = API_KEY;
 
-  // useEffect functions
-  useEffect(() => {
-    getTrendingRecipes();
-  });
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     getRecipes();
   }, [query]);
 
-  // fetch API to get all the recipes
   const getRecipes = async () => {
     // const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     // const response = await fetch(`https://api.spoonacular.com/food/search?query=${query}?&apiKey=${APP_KEY}&number=10`);
@@ -38,7 +30,12 @@ function App() {
     setRecipes(data.searchResults[0].results);
   };
 
-  // fetch API to get Trending recipes
+  const [dietType, setDietType] = useState([]);
+
+  useEffect(() => {
+    getTrendingRecipes();
+  });
+
   const getTrendingRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q&app_id=${APP_ID}&app_key=${ED_APP_KEY}&diet=balanced&limit=5`
